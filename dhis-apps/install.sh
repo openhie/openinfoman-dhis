1205730d4bdbf7fb29529700f881c13e08e746f6
Carl Leitner <litlfred@gmail.com>
2016-01-06 08:02:05 -0500

    updates to the CSD Loader dhis2 app

---
+++ dhis-apps/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+CWD=$(pwd)
+DIRS=("csd-loader" )
+for DIR in ${DIRS[@]}
+do
+    echo "Packaging $DIR"
+    cd $CWD/$DIR  
+    zip -r $CWD/$DIR.zip *
+    echo "Uploading $DIR"
+    curl -k -X POST -u admin:district -F file=@$CWD/$DIR.zip https://localhost/dhis/api/apps
+done
+
