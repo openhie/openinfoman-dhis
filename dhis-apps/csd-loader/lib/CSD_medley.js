080c4b51634c7796c1246f9b6272beef94788a4c
Carl Leitner <litlfred@gmail.com>
2016-06-01 15:56:02 +0000

    updated csd loader app to handle .zip

--- dhis-apps/csd-loader/lib/CSD_medley.js
+++ dhis-apps/csd-loader/lib/CSD_medley.js
@@ -441,6 +441,60 @@ CSDLoader.prototype.Export = function() {
 
 };
 
+//Next function is from here: http://www.henryalgus.com/reading-binary-files-using-jquery-ajax/
+/**
+ * jquery.binarytransport.js
+ *
+ * @description. jQuery ajax transport for making binary data type requests.
+ * @version 1.0 
+ * @author Henry Algus <henryalgus@gmail.com>
+ *
+ */
+ 
+// use this transport for "binary" data type
+$.ajaxTransport("+binary", function(options, originalOptions, jqXHR){
+    // check for conditions and support for blob / arraybuffer response type
+    if (window.FormData && ((options.dataType && (options.dataType == 'binary')) || (options.data && ((window.ArrayBuffer && options.data instanceof ArrayBuffer) || (window.Blob && options.data instanceof Blob)))))
+    {
+        return {
+            // create new XMLHttpRequest
+            send: function(headers, callback){
+		// setup all variables
+                var xhr = new XMLHttpRequest(),
+		url = options.url,
+		type = options.type,
+		async = options.async || true,
+		// blob or arraybuffer. Default is blob
+		dataType = options.responseType || "blob",
+		data = options.data || null,
+		username = options.username || null,
+		password = options.password || null;
+		
+                xhr.addEventListener('load', function(){
+		    var data = {};
+		    data[options.dataType] = xhr.response;
+		    // make callback and send data
+		    callback(xhr.status, xhr.statusText, data, xhr.getAllResponseHeaders());
+                });
+ 
+                xhr.open(type, url, async, username, password);
+		
+		// setup custom headers
+		for (var i in headers ) {
+		    xhr.setRequestHeader(i, headers[i] );
+		}
+		
+                xhr.responseType = dataType;
+                xhr.send(data);
+            },
+            abort: function(){
+                jqXHR.abort();
+            }
+        };
+    }
+});
+
+
 CSDLoader.prototype.ImportSelected = function() {
     this.UpdateStatus('Beginning Data Import');
     var doc = this.form.find('select[name=docs] option:selected').val();
@@ -461,23 +515,23 @@ CSDLoader.prototype.ImportSelected = function() {
 	url:url,
 	data:msg,
 	contentType: 'text/xml',
-	dataType: "xml",
+	dataType: "binary",
 	context: this,
 	cache: false,
+	processData: false,
 	error: function() {
 	    this.Alert('Could not load the selected organisation units');
 	    this.UpdateStatus('Request For Data From ILR Failed');
 	},
-	success: function(xml) {
+	success: function(result) {
 	    this.UpdateStatus('Sending Data For Import To DHIS2');
-	    this.Log("Received\n" +  this.xmlSerializer.serializeToString(xml));
 	    $.ajax({
 	        url: this.BaseURL + '/api/metadata',
 	        method: 'POST',
 	        type: 'POST',
 		context: this,
-		data:  this.xmlSerializer.serializeToString(xml),
-		contentType: 'application/xml',
+		data:  result,
+		contentType: 'application/zip',
 		error: function() {
 		    this.Alert('Could not upload the metadata');
 		    this.UpdateStatus('Data Import On DHIS2 Failed');},
